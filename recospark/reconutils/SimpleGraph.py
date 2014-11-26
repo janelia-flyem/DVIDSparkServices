508244824032e813d896adc8753ed02f13ad5672
Stephen Plaza <plazas@janelia.hhmi.org>
2014-11-26 11:21:44 -0500

    allows option to pass in binary that conforms to API

--- recospark/reconutils/SimpleGraph.py
+++ recospark/reconutils/SimpleGraph.py
@@ -1,4 +1,5 @@
 import dill
+import struct
 
 class SimpleGraph(object):
     def __init__(self, config):
@@ -11,27 +12,71 @@ class SimpleGraph(object):
         pass
         # TODO
 
-    def build_graph(self, label_chunk):
-        # ?! pipe numpy array to string or temporary file
-
-        #data = numpy.getbuffer(label_volume)
-        #return str(data)
-
-        # ?! read executable back from string
-
-        # make a dummy vertex and edge results for now
+    def vol2string(self, label_chunk):
         import numpy
-        vertices = numpy.unique(label_chunk)
+        data = numpy.getbuffer(label_chunk)
+        x,y,z = label_chunk.shape
+
+        # pack little endian 64-bit coordinate numbers
+        coord_data = '<QQQ'
+        coord_bin = struct.pack(coord_data, x, y, z)
+        return str(coord_bin) + str(data)
+
+    def string2graph(self, graphstr):
+        offset = 0
         
+        fout = open("worker.out", 'w')
+
+        # get number of vertices
+        num_verts,  = struct.unpack_from('<Q', graphstr, offset)
+        offset += 8
         elements = []
-        for iter1 in range(len(vertices)):
-            vert = vertices[iter1]
-            elements.append(((vert, -1), 1))
-            if ((vert % 50) == 0) and (iter1 < (len(vertices)-1)):
-                elements.append(((vert, vertices[iter1+1]), 3))
+
+
+        # weight should be a double
+        for vertex in range(0, num_verts):
+            vert1, weight = struct.unpack_from('<Qd', graphstr, offset)
+            offset += 16
+            elements.append(((vert1, -1), weight)) 
+
+        # get number of edges
+        num_edges, = struct.unpack_from('<Q', graphstr, offset)
+        offset += 8
+        
+        fout.write(str(num_edges) + "\n")
+        fout.close()
+
+        # weight should be a double
+        for edge in range(0, num_edges):
+            vert1, vert2, weight = struct.unpack_from('<QQd', graphstr, offset)
+            offset += 24
+            elements.append(((vert1, vert2), weight)) 
 
         return elements
 
+
+    def build_graph(self, label_chunk):
+        if self.external_prog == "":
+            # TODO: default slow python implementation ??
+            #return []
+            import numpy
+            vertices = numpy.unique(label_chunk)
+            
+            elements = []
+            for iter1 in range(len(vertices)):
+                vert = vertices[iter1]
+                elements.append(((vert, -1), 1))
+                if ((vert % 50) == 0) and (iter1 < (len(vertices)-1)):
+                    elements.append(((vert, vertices[iter1+1]), 3))
+
+            return elements
+        else:
+            # call program that expects label stdin and produces graph stdout
+            from subprocess import Popen, PIPE, STDOUT
+            p = Popen([self.external_prog], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+            graphstr = p.communicate(input=self.vol2string(label_chunk))[0]
+            return self.string2graph(graphstr) 
+        
     def is_vertex(self, element_pair):
         edge, value = element_pair
         v1, v2 = edge
