978d677510ee00eb1385223ba091b7ffb3e65d65
Steve Plaza <plazas@janelia.hhmi.org>
2015-05-21 16:02:40 -0400

    fixes problem with ingest gray regression -- the input images were malformed

--- integration_tests/test_ingest/checkoutput.py
+++ integration_tests/test_ingest/checkoutput.py
@@ -10,7 +10,7 @@ cmp = filecmp.dircmp(path1, path2)
 cmp.report_full_closure()
 
 def find_diff_files(cmp):
-    if len(cmp.left_only) > 0 or len(cmp.right_only) > 0 or len(cmp.common_funny) > 0: #or len(cmp.diff_files) > 0:
+    if len(cmp.left_only) > 0 or len(cmp.right_only) > 0 or len(cmp.common_funny) > 0 or len(cmp.diff_files) > 0:
         exit(-1)
     for sub_cmp in cmp.subdirs.values():
         find_diff_files(sub_cmp)
