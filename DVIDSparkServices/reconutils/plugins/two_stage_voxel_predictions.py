28506ac9248dbab754b62983b5ce36eb82f2ae6b
Stuart Berg <bergs@janelia.hhmi.org>
2016-01-08 15:43:29 -0500

    ilastik-two-stage: Each job will now write to its own logfile.

--- DVIDSparkServices/reconutils/plugins/two_stage_voxel_predictions.py
+++ DVIDSparkServices/reconutils/plugins/two_stage_voxel_predictions.py
@@ -139,15 +139,20 @@ def run_ilastik_stage(stage_num, ilp_path, input_vol, mask, output_path,
     args.project = ilp_path
     args.readonly = True
 
-    # By default, all ilastik processes duplicate their console output to ~/.ilastik_log.txt
-    # Obviously, having all spark nodes write to a common file is a bad idea.
-    # The "/dev/null" setting here is recognized by ilastik and means "Don't write a log file"
-    args.logfile = logfile
-
     # The process_name argument is prefixed to all log messages.
     # For now, just use the machine name and a uuid
     # FIXME: It would be nice to provide something more descriptive, like the ROI of the current spark job...
     args.process_name = platform.node() + "-" + str(uuid.uuid1()) + "-" + str(stage_num)
+
+    # To avoid conflicts between processes, give each process it's own logfile to write to.
+    if logfile != "/dev/null":
+        base, ext = os.path.splitext(logfile)
+        logfile += base + '.' + args.process_name + ext
+
+    # By default, all ilastik processes duplicate their console output to ~/.ilastik_log.txt
+    # Obviously, having all spark nodes write to a common file is a bad idea.
+    # The "/dev/null" setting here is recognized by ilastik and means "Don't write a log file"
+    args.logfile = logfile
 
     print "loading shell"
 
