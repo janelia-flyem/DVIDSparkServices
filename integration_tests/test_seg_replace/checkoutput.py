3f1fb2128915b755bf390e574f401389ad9d0bbf
Stuart Berg <bergs@janelia.hhmi.org>
2016-01-20 13:25:17 -0500

    test_seg_replace: Added checkoutput.py to verify that body IDs aren't insanely high.

---
+++ integration_tests/test_seg_replace/checkoutput.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+import sys
+import json
+import numpy as np
+from DVIDSparkServices.reconutils.misc import compute_vi
+from libdvid import DVIDNodeService
+
+dirpath = sys.argv[1]
+
+with open(dirpath + "/temp_data/config.json") as f:
+    config = json.load(f)
+
+node_service = DVIDNodeService(str(config['dvid-info']['dvid-server']), str(config['dvid-info']['uuid']))
+test_segmentation_xyz = node_service.get_labels3D(str(config['dvid-info']['segmentation-name']), (256, 256, 256), (0,0,0))
+
+unique_labels = np.unique(test_segmentation_xyz)
+if unique_labels.max() > 1e9:
+    print("DEBUG: FAIL: Replace test produced very high label values. Max was {}".format(unique_labels.max()))
+    sys.exit(1)
+
+print("DEBUG: Body replace test passed.")
+sys.exit(0)
