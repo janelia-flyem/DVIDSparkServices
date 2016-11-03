ccbdfeacb2a7d8fe86a13233e6c266c7477b4a82
Stuart Berg <bergs@janelia.hhmi.org>
2016-11-03 16:21:18 -0400

    ConnectedComponents: Added the skeleton of an integration test (it doesn't check results, but does get code coverage and therefore detects crashes)

--- integration_tests/launch_tests.py
+++ integration_tests/launch_tests.py
@@ -215,6 +215,7 @@ def run_tests(test_dir, uuid1, uuid2, selected=[], stop_after_fail=True):
     tests["test_seg_rollback"] = "CreateSegmentation"
     tests["test_seg_with_roi"] = "CreateSegmentation"
 
+    tests["test_cc"] = "ConnectedComponents"
     tests["test_comp"] = "EvaluateSeg"
     tests["test_graph"] = "ComputeGraph"
     tests["test_ingest"] = "IngestGrayscale"
