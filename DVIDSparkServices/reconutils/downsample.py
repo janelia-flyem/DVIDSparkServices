1e2fd3708e3ce74ce955ca8879d478ad48d62465
Stuart Berg <bergs@janelia.hhmi.org>
2017-03-27 10:27:24 -0400

    Added ND label downsampling function

--- DVIDSparkServices/reconutils/downsample.py
+++ DVIDSparkServices/reconutils/downsample.py
@@ -3,6 +3,7 @@
 import numpy as np
 from scipy import ndimage
 from numba import jit
+from skimage.util import view_as_blocks
 
 def downsample_raw(data, numlevels=1):
     """Downsamples the given numpy array.
@@ -50,8 +51,7 @@ def downsample_3Dlabels(data, numlevels=1):
     if len(data.shape) != 3:
         raise ValueError("Only supports 3D arrays")
 
-    res = []
-    sortarray = range(8)
+    res = [0]
     for level in range(numlevels):
         # init new data from last shape in pyramid
         zmax, ymax, xmax = data.shape
@@ -104,6 +104,85 @@ def downsample_3Dlabels(data, numlevels=1):
         res.append(data2)
         data = data2
 
-    return res
+    return res[1:]
 
 
+def downsample_labels_nd(a, factor):
+    """
+    Downsample the given ND array by binning the pixels into bins
+    sized according to the downsampling 'factor'.
+    
+    Each bin is reduced to one pixel in the output array, whose value
+    is the most common element from the bin.
+
+    The blockshape can be given by 'factor', which must be either an int or tuple.
+    If factor is tuple, it defines the blockshape.
+    If factor is an int, then the blockshape will be square (or cubic, etc.)
+    """
+    @jit(nopython=True)
+    def _flat_mode_destructive(data):
+        """
+        Given a flat array, return the mode.
+        Beware: Overwrites flat_data.
+        
+        Note: We could have used scipy.stats.mode() here,
+              but that implementation is insanely slow for large arrays,
+              especially if there are many label values in the array.
+        """
+        data.sort()
+        diff = np.diff(data)
+        diff_bool = np.ones((len(diff)+2,), dtype=np.uint8)
+        diff_bool[1:-1] = (diff != 0)
+    
+        diff_nonzero = diff_bool.nonzero()[0]
+        run_lengths = diff_nonzero[1:] - diff_nonzero[:-1]
+        max_run = np.argmax(run_lengths)
+        return data[diff_nonzero[max_run]]
+    
+    @jit(nopython=True)
+    def _blockwise_modes(data, output_shape):
+        """
+        Given an array of shape = output_shape + block_shape,
+        compute the mode of each block.
+        """
+        assert len(output_shape) == len(data.shape) / 2
+        
+        # This assertion not allowed in nopython mode, but it's correct.
+        #assert output_shape == data.shape[:len(data.shape)] 
+        
+        modes = np.zeros(output_shape, data.dtype)
+        for block_index in np.ndindex(output_shape):
+            flat_block_data = data[block_index].copy().reshape(-1)
+            modes[block_index] = _flat_mode_destructive(flat_block_data)
+        return modes
+
+    # If the factor is an int, convert it to a blockshape.
+    if np.issubdtype(type(factor), np.integer):
+        blockshape = (factor,)*a.ndim
+    else:
+        blockshape = factor
+
+    assert not (np.array(a.shape) % blockshape).any(), \
+        "Downsampling factor must divide cleanly into array shape"
+
+    v = view_as_blocks(a, blockshape)
+    return _blockwise_modes(v, v.shape[:a.ndim])
+
+if __name__ == "__main__":
+    a = np.random.randint(0,4, (4,8)).view(np.uint64)
+    print a
+    print ""
+    print downsample_labels_nd(a, 2)
+    print ""
+
+a = np.random.randint(0,4, (6,8)).view(np.uint64)
+print a
+print ""
+print downsample_labels_nd(a, (3,4))
+print ""
+
+a = np.random.randint(0,4, (4,6,8)).view(np.uint64)
+print a
+print ""
+print downsample_labels_nd(a, (2,3,4))
+print ""
