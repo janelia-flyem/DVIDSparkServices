b39833681af3fde830b406be6b9e313813efcf55
Stuart Berg <bergs@janelia.hhmi.org>
2017-03-27 22:01:19 -0400

    downsampling: Label downsampling function is implemented on top of an arbitrary blockwise reducer function.

--- DVIDSparkServices/reconutils/downsample.py
+++ DVIDSparkServices/reconutils/downsample.py
@@ -3,7 +3,6 @@
 import numpy as np
 from scipy import ndimage
 from numba import jit
-from skimage.util import view_as_blocks
 
 def downsample_raw(data, numlevels=1):
     """Downsamples the given numpy array.
@@ -106,83 +105,206 @@ def downsample_3Dlabels(data, numlevels=1):
 
     return res[1:]
 
-
-def downsample_labels_nd(a, factor):
+@jit(nopython=True)
+def downsample_box( box, block_shape ):
     """
-    Downsample the given ND array by binning the pixels into bins
-    sized according to the downsampling 'factor'.
-    
-    Each bin is reduced to one pixel in the output array, whose value
-    is the most common element from the bin.
-
-    The blockshape can be given by 'factor', which must be either an int or tuple.
-    If factor is tuple, it defines the blockshape.
-    If factor is an int, then the blockshape will be square (or cubic, etc.)
+    Given a box (i.e. start and stop coordinates) and a
+    block_shape (downsampling factor), return the corresponding box
+    in downsampled coordinates.
     """
-    @jit(nopython=True)
-    def _flat_mode_destructive(data):
-        """
-        Given a flat array, return the mode.
-        Beware: Overwrites flat_data.
-        
-        Note: We could have used scipy.stats.mode() here,
-              but that implementation is insanely slow for large arrays,
-              especially if there are many label values in the array.
-        """
-        data.sort()
-        diff = np.diff(data)
-        diff_bool = np.ones((len(diff)+2,), dtype=np.uint8)
-        diff_bool[1:-1] = (diff != 0)
+    assert block_shape.shape[0] == box.shape[1]
+    downsampled_box = np.zeros_like(box)
+    downsampled_box[0] = box[0] // block_shape
+    downsampled_box[1] = (box[1] + block_shape - 1) // block_shape
+    return downsampled_box
+
+def make_blockwise_reducer_3d(reducer_func, nopython=True):
+    """
+    Returns a function that can reduce an array of shape (Z*Bz, Y*By, X*Bx)
+    into an array of shape (Z,Y,X), by dividing the array into shapes of blocks (Bz,By,Bx)
+    and calling the given 'reducer' function on each block.
+
+    The reducer function must return a scalar.
+    Ideally, the reducer should be jit-compileable with numba. If not, set nopython=False.
     
-        diff_nonzero = diff_bool.nonzero()[0]
-        run_lengths = diff_nonzero[1:] - diff_nonzero[:-1]
-        max_run = np.argmax(run_lengths)
-        return data[diff_nonzero[max_run]]
+    See reduce_blockwise(), below, for details regarding the returned function.
     
-    @jit(nopython=True)
-    def _blockwise_modes(data, output_shape):
+    Implemented according to guidelines in numba FAQ:
+    http://numba.pydata.org/numba-doc/dev/user/faq.html#can-i-pass-a-function-as-an-argument-to-a-jitted-function
+    """
+    @jit(nopython=nopython)
+    def _reduce_blockwise_compiled(data, block_shape, data_box, reduced_box):
+        _output_shape = reduced_box[1] - reduced_box[0]
+        output_shape = (_output_shape[0], _output_shape[1], _output_shape[2])
+        output = np.zeros(output_shape, data.dtype)
+
+        for block_index in np.ndindex(*output_shape):
+            # block_bounds = block_shape * ( (block_index, 1+block_index) + reduced_box[0] )
+            block_bounds = np.zeros( (2,3), dtype=np.int32 )
+            block_bounds[0] = block_index
+            block_bounds[1] = 1 + block_bounds[0]
+            block_bounds[:] += reduced_box[0]
+            block_bounds[:] *= block_shape
+            
+            block_bounds[0] = np.maximum(block_bounds[0], data_box[0])
+            block_bounds[1] = np.minimum(block_bounds[1], data_box[1])
+    
+            z0, y0, x0 = block_bounds[0] - data_box[0]
+            z1, y1, x1 = block_bounds[1] - data_box[0]
+            
+            block_data = data[z0:z1, y0:y1, x0:x1]
+
+            bi_z, bi_y, bi_x = block_index
+            output[bi_z, bi_y, bi_x] = reducer_func(block_data)
+        return output
+
+    def reduce_blockwise(data, block_shape, data_box=None):
         """
-        Given an array of shape = output_shape + block_shape,
-        compute the mode of each block.
+        Reduce the given 3D array block-by-block, returning a smaller array of scalars (one per block).
+        
+        Args:
+         data:
+             3D array, whose shape need not be exactly divisible by the block_shape
+         
+         block_shape:
+             tuple (Bz,By,Bx)
+         
+         data_box:
+             bounding box pair: [(z0, y0, x0), (z1, y1, x1)]
+             
+             If block_shape does not cleanly divide into block_shape, blocks on the edge
+             of the full data array will be appropriately truncated before they are sent
+             to the reducer function.  This is true for blocks on *any* side of the volume.
+             
+             It is assumed that blocks are aligned to some global coordinate grid,
+             starting at (0,0,0), but the 'data' array might not be aligned with that grid.
+             For example, the first element of the 'data' array may correspond to voxel (0, 0, 1),
+             and therefore the first block will be smaller than most other blocks in the volume.
         """
-        assert len(output_shape) == len(data.shape) / 2
+        assert data.ndim == 3
+    
+        if data_box is None:
+            data_box = np.array([(0,0,0), data.shape])
+        else:
+            data_box = np.asarray(data_box)
         
-        # This assertion not allowed in nopython mode, but it's correct.
-        #assert output_shape == data.shape[:len(data.shape)] 
+        assert data_box.shape == (2,3)
+             
+        # If the block_shape is an int, convert it to a shape.
+        if np.issubdtype(type(block_shape), np.integer):
+            block_shape = (block_shape, block_shape, block_shape)
         
-        modes = np.zeros(output_shape, data.dtype)
-        for block_index in np.ndindex(output_shape):
-            flat_block_data = data[block_index].copy().reshape(-1)
-            modes[block_index] = _flat_mode_destructive(flat_block_data)
-        return modes
+        block_shape = np.array(block_shape)
+        assert block_shape.shape == (3,)
+        
+        if (block_shape == 1).all():
+            # Shortcut: Nothing to do.
+            return data, data_box.copy()
+        
+        reduced_box = downsample_box(data_box, block_shape)
+        reduced_output = _reduce_blockwise_compiled(data, block_shape, data_box, reduced_box)
+        return reduced_output, reduced_box
 
-    # If the factor is an int, convert it to a blockshape.
-    if np.issubdtype(type(factor), np.integer):
-        blockshape = (factor,)*a.ndim
-    else:
-        blockshape = factor
+    return reduce_blockwise
 
-    assert not (np.array(a.shape) % blockshape).any(), \
-        "Downsampling factor must divide cleanly into array shape"
 
-    v = view_as_blocks(a, blockshape)
-    return _blockwise_modes(v, v.shape[:a.ndim])
+@jit(nopython=True)
+def flat_mode(data, exclude_label=0):
+    """
+    Given an array, flatten it and return the mode, without including
+    the given 'exclude_label', if possible.
+    
+    If (data == exclude_label).all(), then exclude_label is returned.
+    
+    Note: We could have used scipy.stats.mode() here,
+          but that implementation is insanely slow for large arrays,
+          especially if there are many label values in the array.
+    """
+    data = data.copy().reshape(-1)
+    data = data[data != exclude_label]
+    if data.size == 0:
+        return exclude_label
+    
+    data.sort()
+    diff = np.diff(data)
+    diff_bool = np.ones((len(diff)+2,), dtype=np.uint8)
+    diff_bool[1:-1] = (diff != 0)
+
+    diff_nonzero = diff_bool.nonzero()[0]
+    run_lengths = diff_nonzero[1:] - diff_nonzero[:-1]
+    max_run = np.argmax(run_lengths)
+    return data[diff_nonzero[max_run]]
+
+
+downsample_labels_3d = make_blockwise_reducer_3d(flat_mode)
+downsample_binary_3d = make_blockwise_reducer_3d(np.any)
+
 
 if __name__ == "__main__":
-    a = np.random.randint(0,4, (4,8)).view(np.uint64)
+    # These work, too:
+    #
+    # @jit(nopython=True)
+    # def flat_mode_exclude_1(data):
+    #     return flat_mode(data, 1)
+    # downsample_labels_3d = make_blockwise_reducer_3d(flat_mode_exclude_1)
+    # 
+    # from functools import partial
+    # downsample_labels_3d = make_blockwise_reducer_3d(partial(flat_mode, exclude_label=1), nopython=False)
+
+    a = np.random.randint(0,4, (1,4,8)).view(np.uint64)
     print a
     print ""
-    print downsample_labels_nd(a, 2)
+    downsampled, box = downsample_labels_3d(a, 2)
+    assert tuple(box[1] - box[0]) == downsampled.shape
+    print downsampled
+    print ""
+    print box.tolist()
     print ""
 
-a = np.random.randint(0,4, (6,8)).view(np.uint64)
-print a
-print ""
-print downsample_labels_nd(a, (3,4))
-print ""
+    a = np.random.randint(0,4, (1,4,8)).view(np.uint64)
+    print a
+    print ""
+    downsampled, box = downsample_binary_3d(a, 2)
+    assert tuple(box[1] - box[0]) == downsampled.shape
+    print downsampled
+    print ""
+    print box.tolist()
+    print ""
 
-a = np.random.randint(0,4, (4,6,8)).view(np.uint64)
-print a
-print ""
-print downsample_labels_nd(a, (2,3,4))
-print ""
+    a = np.array([[[0,1,0],
+                   [0,0,0]]]).astype(np.uint64)
+    print a
+    print ""
+    downsampled, box = downsample_labels_3d(a, (1,1,3))
+    assert tuple(box[1] - box[0]) == downsampled.shape
+    print downsampled
+    print box.tolist()
+    print ""
+
+    a = np.array([[[0,1,0],
+                   [0,0,0]]]).astype(np.uint64)
+    print a
+    print ""
+    downsampled, box = downsample_binary_3d(a, (1,1,3))
+    assert tuple(box[1] - box[0]) == downsampled.shape
+    print downsampled
+    print box.tolist()
+    print ""
+
+    a = np.random.randint(0,4, (1,6,8)).view(np.uint64)
+    print a
+    print ""
+    downsampled, box = downsample_labels_3d(a, (1,3,4))
+    assert tuple(box[1] - box[0]) == downsampled.shape
+    print downsampled
+    print box.tolist()
+    print ""
+    
+    a = np.random.randint(0,4, (4,6,8)).view(np.uint64)
+    print a
+    print ""
+    downsampled, box = downsample_labels_3d(a, (2,3,4))
+    assert tuple(box[1] - box[0]) == downsampled.shape
+    print downsampled
+    print box.tolist()
+    print ""
