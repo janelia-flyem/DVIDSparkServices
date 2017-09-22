48c1993d901991265856903fc2dfe4369b1edcc5
Stuart Berg <bergs@janelia.hhmi.org>
2017-09-22 18:32:44 -0400

    auto_retry: Added predicate argument

--- unit_tests/test_auto_retry.py
+++ unit_tests/test_auto_retry.py
@@ -8,10 +8,13 @@ class TestAutoRetry(unittest.TestCase):
 
     def _check_counter(self):
         self.COUNTER -= 1
-        assert self.COUNTER == 0
+        assert self.COUNTER == 0, f'counter is {self.COUNTER}'
 
     def test_failed_retry(self):
-        should_fail = auto_retry(2, 0.0)(self._check_counter)
+        
+        @auto_retry(2, 0.0)
+        def should_fail():
+            self._check_counter()
         
         try:
             should_fail()
@@ -21,12 +24,44 @@ class TestAutoRetry(unittest.TestCase):
             assert False, "should_fail() didn't fail!"
 
     def test_successful_retry(self):        
-        should_succeed = auto_retry(3, 0.0)(self._check_counter)
+
+        @auto_retry(3, 0.0)
+        def should_succeed():
+            self._check_counter()
     
         try:
             should_succeed()
         except AssertionError:
             assert False, "should_succeed() didn't succeed!"
+
+    def test_failed_with_predicate(self):
+        def predicate(ex):
+            return '1' in ex.args[0]
+
+        @auto_retry(2, 0.0, predicate=predicate)
+        def should_fail():
+            self._check_counter()
+        
+        try:
+            should_fail()
+        except AssertionError:
+            pass
+        else:
+            assert False, "should_fail() didn't fail!"
+
+    def test_success_with_predicate(self):
+        def predicate(ex):
+            return '1' in ex.args[0] or '2' in ex.args[0]
+        
+        @auto_retry(3, 0.0, predicate=predicate )
+        def should_succeed():
+            self._check_counter()
+        
+        try:
+            should_succeed()
+        except AssertionError:
+            assert False, "should_succeed() didn't succeed!"
+
 
 if __name__ == "__main__":
     unittest.main()
