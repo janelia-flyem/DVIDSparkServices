8cda7c3d7a1046d31f22ae2566ec5d5496f1a3e0
Stuart Berg <bergs@janelia.hhmi.org>
2017-10-17 22:14:11 -0400

    brainmaps: Added change stack equivalence getters

--- DVIDSparkServices/io_util/brainmaps.py
+++ DVIDSparkServices/io_util/brainmaps.py
@@ -1,5 +1,7 @@
 import os
 import json
+from itertools import chain
+
 from httplib2 import Http
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -219,22 +221,127 @@ class BrainMapsVolume:
         return box
 
 
-def fetch_json(http, url):
+    def get_equivalence_group(self, segment_id):
+        """
+        Return the set of segments (supervoxels) that are in the same group as the given segment_id.
+        """
+        assert self.change_stack_id, "No equivalences: This volume has no change_stack_id"
+        url = f'{BRAINMAPS_BASE_URL}/changes/{self.project}:{self.dataset}:{self.volume_id}/{self.change_stack_id}/equivalences:getgroups'
+
+        # Technically we could give a list here, but we just ask for one at a time.
+        equivalences = fetch_json(self.http, url, body={'segmentId': [segment_id]})
+        
+        # json data is
+        # 
+        # {
+        #   'groups':
+        #   [
+        #     {
+        #       'groupMembers':
+        #       [
+        #         '411313',
+        #         '828042',
+        #         '1239392',
+        #         ...
+        #       ]
+        #     },
+        #     ...
+        #   ]
+        # }
+        
+        # Convert from string to int
+        siblings = set(map(int, equivalences["groups"][0]["groupMembers"]))
+        assert segment_id in siblings, \
+            f"Expected query segment ({segment_id}) to be in its own equivalence set: {siblings}"
+
+        return siblings
+
+
+    def get_all_equivalence_groups(self):
+        """
+        Like get_equivalences(), but returns results for all groups.
+        
+        Return a dictionary of { group_1 : [segment_1, segment_2, ...],
+                                 group_2 : [segment_10, segment_11, ...],
+                                 ... }
+        """
+        assert self.change_stack_id, "No equivalences: This volume has no change_stack_id"
+
+        # There is no API for getting all groups, so we have to get the
+        # full set of edges and run connected components ourselves.
+        import networkx as nx
+        
+        all_edges = self.get_equivalence_edges()
+        g = nx.Graph()
+        g.add_edges_from(all_edges)
+        
+        groups = {}
+        for segment_set in nx.connected_components(g):
+            # According to Jeremy, group_id == the min segment_id of the group.
+            groups[min(segment_set)] = list(sorted(segment_set))
+
+        return groups
+
+
+    def get_equivalence_edges(self, segment_id=None):
+        """
+        Get the merged edges for the group that owns the given segment as a numpy array [[s1, s2], [s1, s2], ...].
+        If segment_id is None, return all equivalence edges in the entired volume.
+        """
+        assert self.change_stack_id, "No equivalences: This volume has no change_stack_id"
+
+        url = f'{BRAINMAPS_BASE_URL}/changes/{self.project}:{self.dataset}:{self.volume_id}/{self.change_stack_id}/equivalences:list'
+        if segment_id is None:
+            edges_json = fetch_json(self.http, url, body={})
+        else:
+            edges_json = fetch_json(self.http, url, body={'segmentId': segment_id})
+
+        # Data comes back in this format:
+        # 
+        # { 'edge': 
+        #   [ 
+        #     {'first': '411313', 'second': '1033743'},
+        #     {'first': '411313', 'second': '1239364'},
+        #     {'first': '411313', 'second': '1442313'},
+        #     ...
+        #   ]
+        # }
+
+        num_edges = len(edges_json['edge'])
+        firsts = (edge['first'] for edge in edges_json['edge'])
+        seconds = (edge['second'] for edge in edges_json['edge'])
+
+        edges_flat = np.fromiter(chain(firsts, seconds), np.uint64, 2*num_edges)
+        edges = edges_flat.reshape((2,-1)).transpose()
+        return edges
+
+
+def fetch_json(http, url, body=None):
     """
     Fetch JSON data from a BrainMaps API endpoint.
     
     Args:
         http: Authenticated httplib2.Http object.
         url: Full url to the endpoint.
+        body: If the endpoint requires parameters in the body,
+              give them here (as a dict). Forces method to be POST.
     
     Examples:
     
         projects = fetch_json(http, f'{BRAINMAPS_BASE_URL}/projects')
         volume_list = fetch_json(http, f'{BRAINMAPS_BASE_URL}/volumes')
     """
-    response, content = http.request(url)
+    if body is None:
+        method = "GET"
+    else:
+        method = "POST"
+        if not isinstance(body, (str, bytes)):
+            body = json.dumps(body, cls=NumpyConvertingEncoder)
+
+    response, content = http.request(url, method, body)
     if response['status'] != '200':
         raise RuntimeError(f"Bad response ({response['status']}):\n{content.decode('utf-8')}")
+
     return json.loads(content)
 
 
@@ -264,3 +371,22 @@ def fetch_subvol_data(http, project, dataset, volume_id, corner_xyz, size_xyz, s
     if response['status'] != '200':
         raise RuntimeError(f"Bad response ({response['status']}):\n{content.decode('utf-8')}")
     return content
+
+
+class NumpyConvertingEncoder(json.JSONEncoder):
+    """
+    Encoder that converts numpy arrays and scalars
+    into their pure-python counterparts.
+    
+    (No attempt is made to preserve bit-width information.)
+    
+    Usage:
+    
+        >>> d = {"a": np.arange(3, dtype=np.uint32)}
+        >>> json.dumps(d, cls=NumpyConvertingEncoder)
+        '{"a": [0, 1, 2]}'
+    """
+    def default(self, o):
+        if isinstance(o, (np.ndarray, np.number)):
+            return o.tolist()
+        return super().default(o)
