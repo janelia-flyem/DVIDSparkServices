66fba5a03a3943b0e0b977d7c535f8aa235e9b1d
Stuart Berg <bergs@janelia.hhmi.org>
2018-02-01 10:39:53 -0500

    Timer: Optionally display a log message

--- DVIDSparkServices/util.py
+++ DVIDSparkServices/util.py
@@ -103,12 +103,17 @@ class NumpyConvertingEncoder(json.JSONEncoder):
         return super().default(o)
 
 @contextlib.contextmanager
-def Timer():
+def Timer(msg=None, logger=None):
+    if msg:
+        logger = logger or logging.getLogger(__name__)
+        logger.info(msg + '...')
     result = _TimerResult()
     start = time.time()
     yield result
     result.seconds = time.time() - start
     result.timedelta = timedelta(seconds=result.seconds)
+    if msg:
+        logger.info(msg + f' took {result.timedelta}')
 
 class _TimerResult(object):
     seconds = -1.0
